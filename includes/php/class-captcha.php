20042580706d2a118821b00e2319ba54b6428a7e
Alexander W Clark <primevalbully@gmail.com>
2014-07-17 19:15:39 -0700

    Updated Path Links

--- includes/php/class-captcha.php
+++ includes/php/class-captcha.php
@@ -23,7 +23,7 @@
                         id="siimage" 
                         align="left" 
                         alt="Are you human or a bot" 
-                        src="includes/php/securimage_show.php?sid=<?php echo md5(time()) ?>">
+                        src="securimage_show.php?sid=<?php echo md5(time()) ?>">
                       <object 
                         classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" 
                         codebase="https://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=9,0,0,0" 
@@ -35,11 +35,11 @@
                         id="SecurImage_as3">
                         <param name="allowScriptAccess" value="sameDomain">
                         <param name="allowFullScreen" value="false">
-                        <param name="movie" value="includes/php/securimage_play.swf?audio=securimage_play.php&amp;bgColor1=#777&amp;bgColor2=#fff&amp;iconColor=#000&amp;roundedCorner=5">
+                        <param name="movie" value="securimage_play.swf?audio=securimage_play.php&amp;bgColor1=#777&amp;bgColor2=#fff&amp;iconColor=#000&amp;roundedCorner=5">
                         <param name="quality" value="high">			
                         <param name="bgcolor" value="#000000">
                         <embed 
-                          src="includes/php/securimage_play.swf?audio=securimage_play.php&bgColor1=#777&bgColor2=#fff&iconColor=#000&roundedCorner=5" 
+                          src="securimage_play.swf?audio=securimage_play.php&bgColor1=#777&bgColor2=#fff&iconColor=#000&roundedCorner=5" 
                           width="28" 
                           type="application/x-shockwave-flash" 
                           height="28" 
@@ -57,10 +57,10 @@
                         tabindex="-1" 
                         href="#" 
                         title="Refresh Captcha Image" 
-                        onClick="document.getElementById('siimage').src = 'includes/php/securimage_show.php?sid=' + Math.random(); return false">
+                        onClick="document.getElementById('siimage').src = 'securimage_show.php?sid=' + Math.random(); return false">
                         <img 
                           id="refreshIcon"
-                          src="includes/images/refreshIcon30.png" 
+                          src="../images/refreshIcon30.png" 
                           alt="Reload Captcha"
                           border="0" 
                           onClick="this.blur()" 
@@ -88,7 +88,7 @@
                 </div><!--End of cunt Div-->
                 <?php
                   } else { //form is posted
-                  include("includes/php/securimage.php");
+                  include("securimage.php");
                   $img = new Securimage();
                   $valid = $img->check($_POST['code']);
                   }
